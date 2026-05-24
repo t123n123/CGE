4fa65161bc135d498edc7c1d94615e03c61aa1ec
Mihai Spinei <spineim22@gmail.com>
2026-05-24 23:44:29 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Minimal set of features
+// - Draw cards
+// - Play cards (for now without costs)
+// - Choose targets
+// - React to events (example: when another minion is summoned, do something)
+
+fn main() {}
